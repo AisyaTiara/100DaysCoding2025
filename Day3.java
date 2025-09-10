583e9be90e8f6fc81312fb213c293e0c01312c3d
Aisya Tiara <aisyatiara0311@gmail.com>
2025-09-10 16:52:46 +0800

    Update Day3.java

--- Day3.java
+++ Day3.java
@@ -1,19 +1,21 @@
-package pkg100days;
-public class Day3 {
-    public static void main(String[] args) {
-     //untuk komentar 1 baris    
-     //komentar untuk menampilkan teks      
-       System.out.println("hari ke3"); 
-       
-     //komentar untuk menampilkan penjumlahan menggunakan tipe data int
-       int angka = 4;
-       System.out.println("angka : " + (angka + 3));
-       
-    //ini komentar untuk teks panjang       
-    /*Semoga saya bisa menyelesaikan tantangan 100 Days of Coding 
-       dengan baik dan konsisten. Saya berharap setiap hari yang 
-       saya lalui dapat menjadi pengalaman berharga untuk menambah 
-       pengetahuan, keterampilan, dan kedisiplinan dalam dunia pemrograman.*/       
-       System.out.println("semoga konsisten ngoding");
-    }
+// Nama kelas harus sama dengan nama file (aisya.java)
+public class aisya {
+  // Method utama yang akan dijalankan pertama kali
+  public static void main (String [] args) { 
+
+    // Membuat variabel untuk menyimpan umur
+    int umur = 18;
+    // Membuat variabel untuk menyimpan nama
+    String nama = "Aisyah";
+
+    // Menampilkan teks dengan format: Nama: Aisyah, Umur: 18
+    System.out.printf("Nama: %s, Umur: %d", nama, umur);
+
+    // Memberi baris kosong
+    System.out.println(" ");
+    
+    // Menampilkan teks "Halo Dunia" dengan kombinasi print dan println
+    System.out.print("Halo");
+    System.out.println(" Dunia ");
+  }
 }
